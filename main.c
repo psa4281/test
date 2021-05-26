3ba6c43ca1e6756e3f942b92704ddb4db69729ad
PC_10\user <psa4281@naver.com>
2021-05-26 19:27:36 +0900

    이름 및 출력

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main() {
+	char name[10];
 	int age;
+	
+	printf("당신의 이름을 입력하세요 : ");
+	scanf("%d",&name);
+	
 	printf("당신의 나이를 입력하세요 : ");
 	scanf("%d",&age);
-	printf("우와 당신은 %d살이군요!",age);
+	
+	printf("우와 당신은 %d살이군요!",age,name);
 }
